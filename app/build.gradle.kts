9d30c62264211827897dce67ff4d1283be573aa9
Aizen741 <rahulsivadas2009@gmail.com>
2023-10-21 09:35:22 -0400

    Version 1.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.rahulpanikar_midterm_studentgradeapp_8837922"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rahulpanikar_midterm_studentgradeapp_8837922"
